d2f42e5c2dd3f6c7d25bd3a13c290913d092457a
AdmErHad <46301586+AdmErHad@users.noreply.github.com>
2019-08-18 22:04:33 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 const _Btn = document.querySelector(".gn-ham"),
-_Sidebar = document.querySelector(".gn-sidebar"),
+_menu = document.querySelector(".gn-menu"),
+_list = document.querySelector(".gn-list"),
 _botElement = document.querySelector(".gn-ham-bot"),
 _topElement = document.querySelector(".gn-ham-top");
 
 function _toggleClass() {
     document.body.classList.toggle("curtain");
     window.scrollTo(0,0);
-    _Sidebar.classList.toggle("opened");
+    _menu.classList.toggle("opened");
+    _list.classList.toggle("opened");
     _topElement.classList.toggle("active");
     _botElement.classList.toggle("active");
 };
