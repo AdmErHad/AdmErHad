a3493e86e4bfb7dbd49c434e16ec80898c5068e7
AdmErHad <46301586+AdmErHad@users.noreply.github.com>
2019-08-18 14:31:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,14 @@
-$(document).ready(function(){
-    let hamburger = $("#hamburger");
-    let menu = $("#menu");
-    let elements = $("#nav-elements");
-    hamburger.on("click",function(){
-        hamburger.children().toggleClass("black")
-        hamburger.children("#ham-middle").toggleClass("none")
-        hamburger.children("#ham-top").toggleClass("rotateM")
-        hamburger.children("#ham-bottom").toggleClass("rotateP")
-        menu.toggleClass("hidden shown");
-        elements.toggleClass("hidden shown");
-    })
-})
+const _Btn = document.querySelector(".gn-ham"),
+_Sidebar = document.querySelector(".gn-sidebar"),
+_botElement = document.querySelector(".gn-ham-bot"),
+_topElement = document.querySelector(".gn-ham-top");
 
+function _toggleClass() {
+    document.body.classList.toggle("curtain");
+    window.scrollTo(0,0);
+    _Sidebar.classList.toggle("opened");
+    _topElement.classList.toggle("active");
+    _botElement.classList.toggle("active");
+};
 
-
-jQuery(document).ready(function($) {
-  $('a[href^="#"]').bind('click.smoothscroll',function (e) {
-    e.preventDefault();
-    var target = this.hash,
-        $target = $(target);
-
-    $('html, body').stop().animate( {
-      'scrollTop': $target.offset().top
-    }, 900, 'swing', function () {
-      window.location.hash = target;
-    } );
-  } );
-} );
-
-
-
-function parallax(){
-    
-var FormBg = document.getElementById("form-bg");
-    FormBg.style.transform = 'translateY('+ -((window.pageYOffset/2.5)-800)+'px)';
-    
-}
-
-window.addEventListener("scroll", parallax, false);
-
-
-
-
-
-
-
-
-
-
-
-
-
+_Btn.addEventListener("click", _toggleClass);
